2b48e9629651f076f278cedf8d32a8ad8f8d1a6b
Wumer <3877467+Wumer@users.noreply.github.com>
2018-04-12 00:32:05 -0700

    Update Spider-Man.h

--- Spider-Man.h
+++ Spider-Man.h
@@ -2,5 +2,11 @@
 #include hero
 
 //a villain class object named Green Goblin
+villain Green_Goblin
+Green_Goblin.title = "Green Goblin";
+Green_Goblin.identity = "Norman Osborn";
 
 //a hero class object named Spider-Man
+hero Spider-Man
+Spider-Man.title = "Spider-Man";
+Spider-Man.identity = "Peter Parker";
