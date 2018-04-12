0a062630fbb99fe5625aec5be2f688890915f2e6
Wumer <3877467+Wumer@users.noreply.github.com>
2018-04-12 01:02:23 -0700

    Update character.h

--- character.h
+++ character.h
@@ -7,16 +7,21 @@ class character
   //who is this person, and where are they from
   string title; //Superhero name, like Iron Man, Green Goblin, or Doctor Doom
   string identity; //Actual name, like Tony Stark, Norman Osborn, Harry Osborn, or Victor Von Doom
-  string era; //The time this version of the character is from
+  string era = "n/a"; //The time this version of the character is from
   string franchise; //This character's home franchise, like Iron Man, Spier-Man, Fantastic Four, Avengers, or X-Men
   
   //what can this person do
   //abilities get some classes
   //status effects/buffs/debuffs get a class
   
-  //Move to own subclass of this class
+  //Move to nemesis class
   //who does this person fight
   string nemesis_name; //for a specific version of a nemesis
   string nemesis_identity; //for all versions of a nemesis
   string nemesis_team; //for a group of nemeses
+  
+  public:
+  void set_title (string name) {title=name;}
+  void set_identity (sting name) {identity=name;}
+  void set_franchise (string name) {franchise=name;}
 };
