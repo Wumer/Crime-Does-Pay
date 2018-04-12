35e283a865b246eae5cf426fe2e50e3cbaf5f352
Wumer <3877467+Wumer@users.noreply.github.com>
2018-04-12 01:04:43 -0700

    Update Spider-Man.h

--- Spider-Man.h
+++ Spider-Man.h
@@ -3,12 +3,12 @@
 
 //a villain class object named Green Goblin
 villain Green_Goblin
-Green_Goblin.title = "Green Goblin";
-Green_Goblin.identity = "Norman Osborn";
-Green_Goblin.franchise = "Spider-Man";
+Green_Goblin.set_title = "Green Goblin";
+Green_Goblin.set_identity = "Norman Osborn";
+Green_Goblin.set_franchise = "Spider-Man";
 
 //a hero class object named Spider-Man
 hero Spider-Man
-Spider-Man.title = "Spider-Man";
-Spider-Man.identity = "Peter Parker";
-Spider-Man.franchise = "Spider-Man";
+Spider-Man.set_title = "Spider-Man";
+Spider-Man.set_identity = "Peter Parker";
+Spider-Man.set_franchise = "Spider-Man";
