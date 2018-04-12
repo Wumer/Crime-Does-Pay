8d56ea17e5e20f52bab47c7a9d2af216c77cdb04
Wumer <3877467+Wumer@users.noreply.github.com>
2018-04-12 00:18:16 -0700

    Create character.h

---
+++ character.h
@@ -0,0 +1,20 @@
+using namespace std;
+class character
+{
+  private:
+  
+  //who is this person, and where are they from
+  string title; //Superhero name, like Iron Man, Green Goblin, or Doctor Doom
+  string identity; //Actual name, like Tony Stark, Norman Osborn, Harry Osborn, or Victor Von Doom
+  string era; //The time this version of the character is from
+  string franchise; //This character's home franchise, like Iron Man, Spier-Man, Fantastic Four, Avengers, or X-Men
+  
+  //what can this person do
+  //abilities get some classes
+  //status effects/buffs/debuffs get a class
+  
+  //who does this person fight
+  string nemesis_name; //for a specific version of a nemesis
+  string nemesis_identity; //for all versions of a nemesis
+  string nemesis_team; //for a group of nemeses
+}
