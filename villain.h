1dfd19cd2b3944abdd7b098ee3c283ee8bd7192e
Wumer <3877467+Wumer@users.noreply.github.com>
2018-04-12 00:51:28 -0700

    Update villain.h

--- villain.h
+++ villain.h
@@ -1,9 +1,9 @@
 #include character.h
-class villain
+class villain: public character
 {
-  private:
+  protected:
   
   //how many helpers do they have, and who are they
   int current_lieutenants;
   int MAX_lieutenants;
-}
+};
