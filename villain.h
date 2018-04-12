91f5da17e2fbb474b97c11b740417da209e6ec75
Wumer <3877467+Wumer@users.noreply.github.com>
2018-04-12 00:09:48 -0700

    Create villain.h

---
+++ villain.h
@@ -0,0 +1,25 @@
+#include character.h
+using namespace std;
+class villain
+{
+  private:
+  
+  //who is this villain, and where are they from
+  string name; //MOVE TO CHARACTER
+  string identity; //MOVE TO CHARACTER
+  string era; //MOVE TO CHARACTER
+  string franchise; //MOVE TO CHARACTER
+  
+  //how many helpers do they have, and who are they
+  int current_lieutenants;
+  int MAX_lieutenants;
+  
+  //what can this villain do
+  //abilities get some classes
+  //status effects/buffs/debuffs get a class
+  
+  //who does this villain fight
+  string nemesis_name; //for a specific version of a nemesis
+  string nemesis_identity; //for all versions of a nemesis
+  string nemesis_team; //for a group of nemeses
+}
